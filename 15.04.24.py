73ceca7fcfdfcc2479dcc5facf86707a72aaaa6a
wqpqlsk <valerianersisyan826@gmail.com>
2024-05-06 14:53:31 +0300

    7 practice

--- 15.04.24.py
+++ 15.04.24.py
@@ -23,3 +23,15 @@ def pes2(in_path, out_path):
     c.show()
     c.save('D:\\nersisyan\\pythonProject6\\pes4.jpg')
 pes2('D:\\nersisyan\\pythonProject6\\pes.jpg', 'D:\\nersisyan\\pythonProject6\\pes2.jpg')
+
+def img_mirrow(pes):
+    for i in pes:
+        temp = Image.open(i)
+        temp.show()
+        v = temp.filter(ImageFilter.SHARPEN)
+        v.save('D:\1-MD-15-Nersisyan\\15.04.24\\{i+"pes0.jpg"}')
+img_mirrow(['D:\1-MD-15-Nersisyan\\15.04.24\\pess0.jpg',
+            'D:\1-MD-15-Nersisyan\\15.04.24\\pes1.jpg',
+            'D:\1-MD-15-Nersisyan\\15.04.24\\pes3.jpg',
+            'D:\1-MD-15-Nersisyan\\15.04.24\\pes4.jpg',
+            'D:\1-MD-15-Nersisyan\\15.04.24\\pes5.jpg'])
